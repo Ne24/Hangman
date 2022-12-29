22c1fc4a450ebdc6fc5dab704edbf5e2f55f2d81
Ne24 <106443890+Ne24@users.noreply.github.com>
2022-12-29 02:10:29 +0000

    Added print responses to user input

--- milestone_2.py
+++ milestone_2.py
@@ -17,12 +17,12 @@ while guess != 1 or not(guess.isalpha()):
     try:
         guess = input("Enter a single letter: ")
         if len(guess) == 1 and guess.isalpha():
-            pass
+            print("Good guess")
         elif len(guess) > 1:
-            error_description = "This is more than one letter"    
+            error_description = "Oops invalid input: More than one letter"    
             raise Exception()
         elif not(guess.isalpha()):
-            error_description = "This is not a  letter"   
+            error_description = " Oops invalid input: This is not a letter"   
             raise Exception() 
     except:
         print(error_description)
