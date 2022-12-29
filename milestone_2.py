98252dd7a04afce60955cdc1b7d467fe7fc4082b
Ne24 <106443890+Ne24@users.noreply.github.com>
2022-12-29 02:04:15 +0000

    Getting user input

--- milestone_2.py
+++ milestone_2.py
@@ -8,14 +8,23 @@ word = random.choice(word_list)
 
 print(word)
 
-try:
+guess = None
 
-    guess = input("Enter a single letter: ")
-    if len(guess) == 1:
-        pass
-    elif len(guess) > 1:    
-        raise Exception()
-except:
-    print("This is more than one letter")
 
-    
+
+while guess != 1 or not(guess.isalpha()):
+
+    try:
+        guess = input("Enter a single letter: ")
+        if len(guess) == 1 and guess.isalpha():
+            pass
+        elif len(guess) > 1:
+            error_description = "This is more than one letter"    
+            raise Exception()
+        elif not(guess.isalpha()):
+            error_description = "This is not a  letter"   
+            raise Exception() 
+    except:
+        print(error_description)
+    else:
+        break   
