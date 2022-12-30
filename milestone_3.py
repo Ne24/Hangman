f07b8e610359291a40f2c7c974c5f985b5785ca2
Ne24 <106443890+Ne24@users.noreply.github.com>
2022-12-30 20:02:03 +0000

    Checks if letters match selected word

--- milestone_3.py
+++ milestone_3.py
@@ -1,10 +1,24 @@
+import random
+
+word_list = ["Orange","Apple","Coconut", "Pear", "watermelon"]
+
+print(word_list)
+
+word = random.choice(word_list)
+
+print(word)
+
 
 while True:
 
     try:
         guess = input("Enter a single letter: ")
         if len(guess) == 1 and guess.isalpha():
-            print("Good guess")
+            if guess in word:
+                print(f"Good guess {guess} is in the word!")
+            else:
+                print(f"Sorry {guess} is not in word. Try again.")
+                continue
         elif len(guess) > 1:
             error_description = "Oops invalid input: More than one letter"    
             raise Exception()
