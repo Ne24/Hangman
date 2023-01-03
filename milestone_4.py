9af23d7b1af8ebe0402470446845875c1cb2307f
Ne24 <ATdiscord@outlook.com>
2023-01-03 20:24:51 +0000

    fixes attributes

--- milestone_4.py
+++ milestone_4.py
@@ -1,62 +1,63 @@
 
-import random
+# import random
 
-word_list = ["Orange","Apple","Coconut", "Pear", "watermelon"]
+# word_list = ["Orange","Apple","Coconut", "Pear", "watermelon"]
 
-print(word_list)
+# print(word_list)
 
-word = random.choice(word_list)
+# word = random.choice(word_list)
 
-word = word.lower()
+# word = word.lower()
 
-print(word)
+# print(word)
 
     
-def check_guess(guess):
-    """Checks if guess input is in the word"""
+# def check_guess(guess):
+#     """Checks if guess input is in the word"""
 
-    guess = guess.lower()
+#     guess = guess.lower()
 
-    if guess in word:
-        print(f"Good guess {guess} is in the word!")
-    else:
-        print(f"Sorry {guess} is not in word. Try again.")
-
-
-def ask_for_input():
-    """Asks for guesss and check validity of guess """
-
-    while True:
-
-        try:
-            guess = input("Enter a single letter: ")
-            if len(guess) == 1 and guess.isalpha():
-                pass
-            elif len(guess) > 1:
-                error_description = "Oops invalid input: More than one letter"    
-                raise Exception()
-            elif not(guess.isalpha()):
-                error_description = " Oops invalid input: This is not a letter"   
-                raise Exception() 
-        except:
-            print(error_description)
-        else:
-            break   
-    check_guess(guess)
+#     if guess in word:
+#         print(f"Good guess {guess} is in the word!")
+#     else:
+#         print(f"Sorry {guess} is not in word. Try again.")
 
 
-ask_for_input()
+# def ask_for_input():
+#     """Asks for guesss and check validity of guess """
 
+#     while True:
+
+#         try:
+#             guess = input("Enter a single letter: ")
+#             if len(guess) == 1 and guess.isalpha():
+#                 pass
+#             elif len(guess) > 1:
+#                 error_description = "Oops invalid input: More than one letter"    
+#                 raise Exception()
+#             elif not(guess.isalpha()):
+#                 error_description = " Oops invalid input: This is not a letter"   
+#                 raise Exception() 
+#         except:
+#             print(error_description)
+#         else:
+#             break   
+#     check_guess(guess)
+
+
+# ask_for_input()
+
+import random
 
 class Hangman():
     """"""
-    import random
-
     def __init__(self, word_list, num_lives = 5 ) -> None:
-        
-        self.word = random.choice(self.word_list)
-        self.word_guessed = []
 
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(word_list)
+        self.word_guessed = []
+        
         for x in self.word:
             self.word_guessed.append("_")
         
@@ -83,9 +84,7 @@ class Hangman():
 
             uniquecounter +=1
 
-        self.word_list = word_list
-        self.num_lives = num_lives
+        # self.word_list = word_list
+        # self.num_lives = num_lives
         self.list_of_guesses = []
-    pass
-
-    
+    pass   
