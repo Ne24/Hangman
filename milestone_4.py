920cf6dcd5e6934507c725652fab98cb52306b9e
Ne24 <ATdiscord@outlook.com>
2023-01-03 20:51:27 +0000

    Cleaned up, redundant code removed

--- milestone_4.py
+++ milestone_4.py
@@ -1,54 +1,4 @@
-
-# import random
-
-# word_list = ["Orange","Apple","Coconut", "Pear", "watermelon"]
-
-# print(word_list)
-
-# word = random.choice(word_list)
-
-# word = word.lower()
-
-# print(word)
-
-    
-# def check_guess(guess):
-#     """Checks if guess input is in the word"""
-
-#     guess = guess.lower()
-
-#     if guess in word:
-#         print(f"Good guess {guess} is in the word!")
-#     else:
-#         print(f"Sorry {guess} is not in word. Try again.")
-
-
-# def ask_for_input():
-#     """Asks for guesss and check validity of guess """
-
-#     while True:
-
-#         try:
-#             guess = input("Enter a single letter: ")
-#             if len(guess) == 1 and guess.isalpha():
-#                 pass
-#             elif len(guess) > 1:
-#                 error_description = "Oops invalid input: More than one letter"    
-#                 raise Exception()
-#             elif not(guess.isalpha()):
-#                 error_description = " Oops invalid input: This is not a letter"   
-#                 raise Exception() 
-#         except:
-#             print(error_description)
-#         else:
-#             break   
-#     check_guess(guess)
-
-
-# ask_for_input()
-
 import random
-
 class Hangman():
     """"""
     def __init__(self, word_list, num_lives = 5 ) -> None:
@@ -57,23 +7,12 @@ class Hangman():
         self.num_lives = num_lives
         self.word = random.choice(word_list)
         self.word_guessed = []
+        self.list_of_guesses = []
+        self.num_letters = 0
+        uniquecounter = 0
         
         for x in self.word:
             self.word_guessed.append("_")
-        
-        # count =0 
-        # #while num_lives >= 1:
-        
-        # for y in word:
-        #     if guess == y:
-        #         word_guessed[count] = guess
-        #         count +=1
-        #     else:
-        #         # num_lives -=1
-              
-
-        self.num_letters = 0
-        uniquecounter = 0
 
         for z in self.word:
             if uniquecounter > 0:
@@ -82,9 +21,5 @@ class Hangman():
             else:
                 self.num_letters +=1 
 
-            uniquecounter +=1
-
-        # self.word_list = word_list
-        # self.num_lives = num_lives
-        self.list_of_guesses = []
+            uniquecounter +=1        
     pass   
